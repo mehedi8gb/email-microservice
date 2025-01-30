6ca970b6af47771653af75a24bf17e97c5d3d54c
mehidy <mehidy.gb@gmail.com>
2025-01-30 06:21:32 +0600

    smtp config store method fixed

--- app/Http/Requests/StoreSMTPRequest.php
+++ app/Http/Requests/StoreSMTPRequest.php
@@ -17,6 +17,8 @@ class StoreSMTPRequest extends FormRequest
             'company_id' => 'required|integer|exists:companies,id',
             'host' => 'required|string',
             'port' => 'required|integer',
+            'from_email' => 'required|email',
+            'from_name' => 'nullable|string',
             'username' => 'required|string',
             'password' => 'required|string',
             'encryption' => 'required|string|in:ssl,tls',
--- database/migrations/2025_01_20_145603_create_smtp_configs_table.php
+++ database/migrations/2025_01_20_145603_create_smtp_configs_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->binary('password');
             $table->binary('encryption');
             $table->string('from_email');
-            $table->string('from_name');
+            $table->string('from_name')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
